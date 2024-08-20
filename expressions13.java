9a8ab9ffd008ef59a3570802b5fb378f67baf7bb
Tanishka <tanishka3goyal@gmail.com>
2024-08-19 23:18:41 -0500

    commited

---
+++ expressions13.java
@@ -0,0 +1,15 @@
+public class expressions13 {
+    public static void main(String[] args) {
+      //assign values to variables
+      int val1 =10;
+      int val2= 20;
+      int val3 = 100;
+      //calculating the sum
+      int sum= val1+val2+val3;
+      //calculating the average
+      double avg=sum/3.0;
+      //printing the final answer
+      System.out.println("Average: " + avg);
+
+    }
+}
