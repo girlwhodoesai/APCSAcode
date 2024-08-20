cc96d8302328c7ab9672ea3a637d1954664a4d17
Tanishka <tanishka3goyal@gmail.com>
2024-08-19 22:51:33 -0500

    commited

---
+++ 12variables.java
@@ -0,0 +1,21 @@
+public class java12 {
+    public static void main(String[] args) {
+       // declare variable for name 
+       String name= "Tanishka";
+       // declare varibale for fav num
+       int favNum= 30;
+       // declare variable for avg grade
+       double avgGrade= 95.64;
+       //declare variable for homework done
+       boolean isHomeowrkDone= true;
+       //declare varibale for num of days in month
+       final int numDays= 30;
+
+       // Print the values of the variables
+       System.out.println("Name: " + name);
+       System.out.println("Favorite Number: " + favNum);
+       System.out.println("Average Grade: " + avgGrade);
+       System.out.println("Completed Homework: " + isHomeowrkDone);
+       System.out.println("Days in month: " + numDays);
+    }
+} 
