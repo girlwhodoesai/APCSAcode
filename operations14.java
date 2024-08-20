f27566bbc42bced0b39920ab440ab8a3664bbb3f
Tanishka <tanishka3goyal@gmail.com>
2024-08-20 00:20:57 -0500

    commited

---
+++ operations14.java
@@ -0,0 +1,12 @@
+public class operations14 {
+    public static void main(String[] args) {
+        double waterIntake=0;
+        waterIntake+=0.25; //one glass of water
+        waterIntake+=0.25; //one glass of water
+        waterIntake+=0.25; //one glass of water
+        waterIntake+=0.25; //one glass of water
+        waterIntake+=0.25; //one glass of water
+        waterIntake+=0.25; //one glass of water
+        System.out.println("Total water intake= " + waterIntake + " liters"); 
+    }
+}
