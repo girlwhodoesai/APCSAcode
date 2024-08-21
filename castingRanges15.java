983892aa7093e5e997a24b21e7c9547bab1a1858
Tanishka <tanishka3goyal@gmail.com>
2024-08-20 22:06:08 -0500

    commited

--- castingRanges15.java
+++ castingRanges15.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class castingRanges15 {
     public static void main(String[] args) {
+        //program for converting temp in farenheit to celsius
         Scanner scanner = new Scanner(System.in);
+        // asking the user for temp in farenheit
         System.out.print("Enter temperature in Fahrenheit: ");
         double fahrenheit = scanner.nextDouble();
+        //formula for converting temp from farenheit to celsius
         double celsius = (fahrenheit - 32) * 5 / 9;
         int celsiusRounded = (int)(celsius + 0.5);
+        //printing out the temp in celsius
         System.out.println("Temperature in Celsius: " + celsiusRounded);
     }
 }
